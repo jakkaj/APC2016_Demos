4fa74cf27eee709a0a0d63c9ef64e3eb1ac56b41
Kym Phillpotts <kphillpotts@gmail.com>
2016-08-31 13:35:16 +1000

    ImageSearch Demo
    Traditional Xamarin.iOS and Xamarin.Android app, that uses bing image
    search, cognitive services and cross platform camera

---
+++ ImageSearch/ImageSearch/ImageSearch.Droid/MainActivity.cs
@@ -0,0 +1,112 @@
+﻿using Android.App;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.Widget;
+using ImageSearch.Droid.Adapters;
+using ImageSearch.ViewModel;
+using Acr.UserDialogs;
+using System.Threading.Tasks;
+
+namespace ImageSearch.Droid
+{
+    [Activity(Label = "Image Search", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : BaseActivity
+    {
+        RecyclerView recyclerView;
+        RecyclerView.LayoutManager layoutManager;
+        ImageAdapter adapter;
+        ProgressBar progressBar;
+
+        ImageSearchViewModel viewModel;
+
+        protected override int LayoutResource
+        {
+            get { return Resource.Layout.main; }
+        }
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            viewModel = new ImageSearchViewModel();
+
+            //Setup RecyclerView
+            adapter = new ImageAdapter(this, viewModel);
+            recyclerView = FindViewById<RecyclerView>(Resource.Id.recyclerView);
+            recyclerView.SetAdapter(adapter);
+            layoutManager = new GridLayoutManager(this, 2);
+            recyclerView.SetLayoutManager(layoutManager);
+
+            progressBar = FindViewById<ProgressBar>(Resource.Id.my_progress);
+            progressBar.Visibility = ViewStates.Gone;
+
+            var query = FindViewById<EditText>(Resource.Id.my_query);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            var clickButton = FindViewById<Button>(Resource.Id.my_button);
+            clickButton.Click += async (sender, args) =>
+            {
+                clickButton.Enabled = false;
+                progressBar.Visibility = ViewStates.Visible;
+
+                await viewModel.SearchForImagesAsync(query.Text.Trim());
+
+                progressBar.Visibility = ViewStates.Gone;
+                clickButton.Enabled = true;
+            };
+
+            //Button Click event to get images
+            adapter.ItemClick += async (object sender, ImageAdapterClickEventArgs e) =>
+            {
+                clickButton.Enabled = false;
+                progressBar.Visibility = ViewStates.Visible;
+
+                var result = await viewModel.GetImageDescription(viewModel.Images[e.Position].ImageLink);
+                new AlertDialog.Builder(this)
+                               .SetTitle("Image Analysis")
+                               .SetMessage(result)
+                               .SetNeutralButton("OK", delegate { })
+                               .Show();
+
+                progressBar.Visibility = ViewStates.Gone;
+                clickButton.Enabled = true;
+            };
+
+            UserDialogs.Init(this);
+            SupportActionBar.SetDisplayHomeAsUpEnabled(false);
+            SupportActionBar.SetHomeButtonEnabled(false);
+        }
+
+        // create toolbar menu
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_photo, menu);
+            return base.OnCreateOptionsMenu(menu);
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Resource.Id.action_camera:
+                    SelectImage(true);
+                    break;
+                case Resource.Id.action_pick:
+                    SelectImage(false);
+                    break;
+            }
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private async Task SelectImage(bool UseCamera)
+        {
+            progressBar.Visibility = ViewStates.Visible;
+            await viewModel.TakePhotAsync(UseCamera);
+            progressBar.Visibility = ViewStates.Gone;
+        }
+
+    }
+}
+
---
+++ ImageSearch/ImageSearch/ImageSearch.iOS/ViewController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Foundation;
+using UIKit;
+using ImageSearch.ViewModel;
+using SDWebImage;
+using System.Linq;
+using Acr.UserDialogs;
+
+namespace ImageSearch.iOS
+{
+    public partial class ViewController : UIViewController, IUICollectionViewDataSource, IUICollectionViewDelegate
+    {
+        ImageSearchViewModel viewModel;
+
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            viewModel = new ImageSearchViewModel();
+
+            CollectionViewImages.WeakDataSource = this;
+            CollectionViewImages.AllowsSelection = true;
+
+            CollectionViewImages.Delegate = this;
+
+            //Button Click event to get images
+            ButtonSearch.TouchUpInside += async (sender, args) =>
+            {
+                ButtonSearch.Enabled = false;
+                ActivityIsLoading.StartAnimating();
+
+                await viewModel.SearchForImagesAsync(TextFieldQuery.Text);
+                CollectionViewImages.ReloadData();
+
+                ButtonSearch.Enabled = true;
+                ActivityIsLoading.StopAnimating();
+            };
+
+            // toolbar buttons
+            var cameraButton = new UIBarButtonItem(UIBarButtonSystemItem.Camera, 
+            async (sender, e) =>
+            {
+                await viewModel.TakePhotAsync();
+            });
+
+            var pickButton = new UIBarButtonItem(UIBarButtonSystemItem.Organize,
+            async (sender, e) =>
+            {
+                await viewModel.TakePhotAsync(false);
+            });
+
+            this.NavigationItem.RightBarButtonItems = new UIBarButtonItem[] { cameraButton, pickButton };
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+        [Export("collectionView:didSelectItemAtIndexPath:")]
+        public async void ItemSelected(UICollectionView collectionView, NSIndexPath indexPath)
+        {
+            ActivityIsLoading.StartAnimating();
+
+            string description = await viewModel.GetImageDescription(viewModel.Images[indexPath.Row].ImageLink);
+            UIAlertView alert = new UIAlertView("Image Analysis",
+                                                description, null, "OK", null);
+            alert.Show();
+
+            ActivityIsLoading.StopAnimating();
+        }
+
+        public nint GetItemsCount(UICollectionView collectionView, nint section) =>
+            viewModel.Images.Count;
+
+        public UICollectionViewCell GetCell(UICollectionView collectionView, NSIndexPath indexPath)
+        {
+            var cell = collectionView.DequeueReusableCell("imagecell", indexPath) as ImageCell;
+
+            var item = viewModel.Images[indexPath.Row];
+
+            cell.Caption.Text = item.Title;
+
+            cell.Image.SetImage(new NSUrl(item.ThumbnailLink));
+
+            return cell;
+        }
+
+
+    }
+}
+
